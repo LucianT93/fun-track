32595251a3a2da90f907511317c7e181bf98558b
Georger <rusugeorgevlad@gmail.com>
2022-10-26 18:32:49 +0300

    updated models to include active, created

--- userextend/models.py
+++ userextend/models.py
@@ -2,7 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class UserExtend(User):
-    is_manager=models.BooleanField()
+    is_manager=models.BooleanField(default=False)
     is_employee = models.BooleanField(default=True)
     experience = models.IntegerField(defaul=0)
     gold = models.IntegerField(default=0)
+
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f'{self.first_name}  {self.last_name}'
