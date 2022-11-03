00eb2384a7514aaa04b9ca2e5a8a6bb5900ad10e
LucianT93 <lucianlukas93@gmail.com>
2022-11-03 20:20:15 +0200

    Solved: update task functionality

--- tasks/views.py
+++ tasks/views.py
@@ -40,53 +40,38 @@ def create_task(request):
         task_create.save()
         return redirect('home')
 
-
-
     return render(request, 'tasks/create_task.html', {
         'form': task_create_form,
         'users': users
     })
 
 
-#Create update task functionality for both managers and users, facem un singur formular si controlam ce se afiseaza din form in front end cu if-uri
+# Create update task functionality for both managers and users, facem un singur formular si controlam ce se afiseaza din form in front end cu if-uri
 
 
 def update_task(request, pk):
-    task_to_update = Tasks.objects.get(id=pk) #aici vine id-ul din frontend
+    task_to_update = Tasks.objects.get(id=pk)  # aici vine id-ul din frontend
     task_update_form = TaskCreationForm(instance=task_to_update)
-
     users = UserExtend.objects.all()
     if request.method == 'POST':
         print(request.POST)
 
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-        difficulty = request.POST.get('difficulty')
-        assign_to = int(request.POST.get('assign-to'))
+        task_to_update.name = request.POST.get('name')
+        task_to_update.description = request.POST.get('description')
+        task_to_update.difficulty = request.POST.get('difficulty')
+        task_to_update.assigned_to = int(request.POST.get('assign-to'))
 
-        if difficulty == 'easy':
-            experience = 50
-            currency = 50
-        elif difficulty == 'medium':
-            experience = 100
-            currency = 100
+        if task_to_update.difficulty == 'easy':
+            task_to_update.experience = 50
+            task_to_update.currency = 50
+        elif task_to_update.difficulty == 'medium':
+            task_to_update.experience = 100
+            task_to_update.currency = 100
         else:
-            experience = 150
-            currency = 150
+            task_to_update.experience = 150
+            task_to_update.currency = 150
 
-        task_update, created = Tasks.objects.update_or_create(
-            name=name,
-            description=description,
-            difficulty=difficulty,
-            assigned_to=assign_to,
-            experience=experience,
-            currency=currency,
-            task_creator=UserExtend.objects.get(user_ptr_id=request.user.id),
-        )
-        # print(task_update)
-        task_update.save()
-
-
+        task_to_update.save()
 
     return render(request, 'tasks/update_tasks.html', {
         'form': task_update_form,
