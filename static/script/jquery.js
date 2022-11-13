e73ec540d72daabfc074c95e5ac580af6387c2bd
LucianT93 <lucianlukas93@gmail.com>
2022-11-13 13:03:20 +0200

    Draft: login registration check

--- static/script/jquery.js
+++ static/script/jquery.js
@@ -4,7 +4,7 @@ function show_details(id) {
         url: '/task_detail/' + id,
         dataType: 'json'
     }).done(function (data) {
-        $('#task_detail').load('/task_detail/'+id, data)
+        $('#task_detail').load('/task_detail/' + id, data)
     })
 }
 
@@ -14,3 +14,26 @@ function changeColor() {
         .setProperty('--button-color', color);
     document.getElementById('colorpicker').value = color;
 }
+
+$(document).ready(() => {
+    $('#login_form').submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'POST',
+            url: "login_register/",
+            data: {
+                flag: $("#flag").val(),
+                username: $('#user-login').val(),
+                password: $('#password-login').val(),
+                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+            },
+            success: function (data) {
+                if (data['message'] === 'success') {
+                    location.reload()
+                } else {
+                    $('#login_error').show()
+                }
+            }
+        })
+    })
+})
--- userextend/urls.py
+++ userextend/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from userextend import views
 
 urlpatterns = [
-    # path('register/', views.EmployeeCreateView.as_view(), name='register'),
-    path('login/', views.user_login_registration, name='login'),
+    path('get_login_register/', views.render_login_registration, name='get_login_register'),
+    path('login_register/', views.user_login_registration, name='login_register'),
     path("logout/", views.user_logout, name="logout"),
     path("account/", views.get_account, name='account'),
     path("update_account/", views.update_account, name='update_account'),
